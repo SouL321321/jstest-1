43034e78bbcc1f2b3a10ae0bf27543063b3fc4f6
SouL <battleofcod@gmail.com>
2023-12-03 13:12:58 +0100

    change activity time

--- src/commands/tools/time.js
+++ src/commands/tools/time.js
@@ -1,11 +1,21 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, ActivityType } = require("discord.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("time")
     .setDescription("Return the time!⌚"),
   async execute(interaction, client) {
-    const actualTime = new Date().toLocaleTimeString();
-    interaction.reply(`Time: ${actualTime}`);
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
+    const activity = {
+        name: `⌚Time: ${formattedTime}⌚`,
+        type: ActivityType.DEFAULT,
+    };
+    client.user.setActivity(activity);
+    
+    const currentTime = new Date().toLocaleTimeString();
+    interaction.reply(`Time: ${currentTime}`);
   },
 };
