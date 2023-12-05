d5de664bb5d482fcb5c4a69721aa01715fb5ea31
SouL <battleofcod@gmail.com>
2023-12-05 14:45:49 +0100

    search-wiki.js command (install npm i wikijs)

---
+++ src/commands/tools/search-wiki.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const wiki = require("wikijs").default();
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("search-wiki")
+    .setDescription("Ask Wikipedia!🌍")
+    .addStringOption((option) =>
+      option
+        .setName("query")
+        .setDescription("Search on Wikipedia")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const query = interaction.options.getString("query");
+
+    await interaction.deferReply();
+
+    const search = await wiki.search(query);
+    if (!search.results.length)
+      return await interaction.editReply({
+        content: `Wikipedia doesn't know what you're talking about...`,
+        ephemeral: true,
+      });
+
+    const result = await wiki.page(search.results[0]);
+
+    const summary = await result.summary();
+    const maxWords = 100; 
+
+    const limitedSummary = summary.split(" ").slice(0, maxWords).join(" ");
+
+    const embed = new EmbedBuilder()
+      .setColor("Blue")
+      .setTitle(`Wiki Search: ${result.raw.title}`)
+      .setDescription(`\`\`\`${limitedSummary}\`\`\``);
+
+    if (summary.length > limitedSummary.length) {
+      embed.addFields({
+        name: "READ MORE",
+        value: `[Wikipedia Article](${result.raw.fullurl})`,
+      });
+    }
+
+    await interaction.editReply({ embeds: [embed] });
+  },
+};
