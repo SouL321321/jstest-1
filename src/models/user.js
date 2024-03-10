d9f912452650bc42b9123c0c24a85a2a73fc5485
asddas <battleofcod@gmail.com>
2024-03-10 11:05:46 +0100

    no need user.js in the database for now

--- src/models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  userId: String,
-  password: String,
-});
-
-module.exports = mongoose.model("User", userSchema);
